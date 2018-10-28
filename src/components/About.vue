9d3162a39867305e771386b215911681b059e8a4
cweave <cweav3r@gmail.com>
2018-10-28 00:05:16 -0400

    Commiting files to Github

---
+++ src/components/About.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="about">
+    	<h1>cweave's first stab at Vue.js</h1>
+   		<p>Followed along to <a href="https://coursetro.com/courses/23/Vue-Tutorial-in-2018" target="_blank">Coursetro's Vue.js</a> tutorial.</p>
+		<p>Features:</p>
+		<ul>
+			<li>Vue.js</li>
+			<li>Page routing</li>
+			<li>Vee Validate</li>
+			<li>Importing CSS Animations</li>
+			<li>Importing Font Awesome</li>
+		</ul>
+	</div>
+</template>
